5661da483cac6fe9de71912c6210f7b426876806
Sasha Semenenko <semenenko233249@gmail.com>
2023-08-10 13:28:43 +0300

    Added models to model.py

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=63)
+    content = models.TextField(max_length=500)
+    published_date = models.DateTimeField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self) -> str:
+        return f"{self.title} published at {self.published_date}"
